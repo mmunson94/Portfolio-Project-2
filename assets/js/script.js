f2e90ae9e99668320465006c9ceaae4889611b95
mmunson94 <michaeljackmunson9994@outlook.com>
2023-01-04 22:04:59 +0000

    Add start of questions array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const questions = [
+    {
+        question: "Who is Manchester Uniteds longest serving manager?",
+        options: ["Alex Ferguson", "Matt Busby", "Ron Atkinson", "Jose Mourinho"],
+
+        answerIndex: 0,
+    }
+    {
+        question: "Which of these Manchester United players has not won the Ballon d'or?",
+        options: ["Bobby Charlton", "Eric Cantona", "Cristiano Ronaldo", "George Best"],
+
+        answerIndex: 1,
+    },
+    {
+        question: "In what year was Manchest United founded?",
+        options: ["1904", "1878", "1894", "1910"],
+
+        answerIndex: 1,
+    },
+    [
+        question: "What were Manchester United originally called?",
+        options: ["F.C. United of Manchester", "Manchester North End F.C.", "Northwhich Manchester Villa F.C.", "Newton Heath LYR"],
+
+        answerIndex: 0,
+    ],
+];

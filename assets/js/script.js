573aee64847732639ab1440a33b98e71c8ab3350
mmunson94 <michaeljackmunson9994@outlook.com>
2023-01-08 20:28:43 +0000

    Edit and style question container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
 const questions = [
     {
         question: "Who is Manchester Uniteds longest serving manager?",
-        options: ["Alex Ferguson", "Matt Busby", "Ron Atkinson", "Jose Mourinho"],
+        answers: ["Alex Ferguson", "Matt Busby", "Ron Atkinson", "Jose Mourinho"],
 
         answerIndex: 0,
     },
     {
         question: "Which of these Manchester United players has not won the Ballon d'or?",
-        options: ["Bobby Charlton", "Eric Cantona", "Cristiano Ronaldo", "George Best"],
+        answers: ["Bobby Charlton", "Eric Cantona", "Cristiano Ronaldo", "George Best"],
 
         answerIndex: 1,
     },
     {
         question: "In what year was Manchest United founded?",
-        options: ["1904", "1878", "1894", "1910"],
+        answers: ["1904", "1878", "1894", "1910"],
 
         answerIndex: 1,
     },
     {
         question: "What were Manchester United originally called?",
-        options: ["F.C. United of Manchester", "Manchester North End F.C.", "Northwhich Manchester Villa F.C.", "Newton Heath LYR"],
+        answers: ["F.C. United of Manchester", "Manchester North End F.C.", "Northwhich Manchester Villa F.C.", "Newton Heath LYR"],
 
         answerIndex: 0,
     },
@@ -50,6 +50,7 @@ function startGame() {
 function showNextQuestion() {
     let currentQuestion = questions[currentQuestionIndex]; 
     questionContainer.textContent = currentQuestion.question;
+    
 }
  
 function checkAnswer() {
